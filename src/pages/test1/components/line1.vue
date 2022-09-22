56088623b0908ea5a7358a28aef86cc7af0496d5
chan <2420395206@qq.com>
2022-09-22 10:20:53 +0800

    first

---
+++ src/pages/test1/components/line1.vue
@@ -0,0 +1,112 @@
+<template>
+    <t-space direction="vertical" size="32px" v-if="this.$props.myProps.rowkey===100">
+        <t-form
+            :data="formData"
+            labelWidth="calc(2em + 128px)"
+            :layout="formData.layout"
+            ref="form"
+            @reset="onReset"
+            @submit="onSubmit"
+            scrollToFirstError="smooth"
+        >
+            <t-form-item
+                label="纳税人识别号"
+                name="id"
+                :rules="[{ required: true, message: '纳税人识别号不可为空' }, { validator: this.nameValidator }]"
+            >
+                <t-input v-model="formData.id" clearable placeholder="格式: 001xxx"></t-input>
+            </t-form-item>
+            <t-form-item label="纳税人名称" name="name">
+                <t-input v-model="formData.name" :disabled="formDisabled" placeholder=""></t-input>
+            </t-form-item>
+            <t-form-item label="登记注册类型" name="type">
+                <t-select v-model="formData.type" :options="COLLEGE_OPTIONS" clearable></t-select>
+            </t-form-item>
+            <t-form-item label="主管税务机关" name="government">
+                <t-input v-model="formData.government" :disabled="formDisabled" placeholder=""></t-input>
+            </t-form-item>
+            <t-form-item label="纳税人生产经营地址" name="address">
+                <t-input v-model="formData.address" :disabled="formDisabled" placeholder=""></t-input>
+            </t-form-item>
+            <t-form-item :statusIcon="false" style="margin-left: 80px">
+                <t-space size="10px">
+                    <t-button theme="primary" type="submit">查询</t-button>
+                    <t-button theme="default" variant="base" type="reset">重置</t-button>
+                </t-space>
+            </t-form-item>
+        </t-form>
+    </t-space>
+</template>
+<script>
+const INITIAL_DATA = {
+    layout: "vertical",
+    id: "",
+    name: "",
+    type: 1,
+    government: "",
+    address: ""
+}
+const COLLEGE_OPTIONS = [
+    { label: "类型 A", value: 1 },
+    { label: "类型 B", value: 2 },
+    { label: "类型 C", value: 3 }
+]
+
+// const state = []
+const mark = ""
+
+export default {
+    props:[
+        'myProps'
+    ],
+    data() {
+        return {
+            formData: { ...INITIAL_DATA },
+            formDisabled: true,
+            COLLEGE_OPTIONS,
+            dataLoading: false
+        }
+    },
+    mounted() {
+        console.log("rowIIIId",this.$props)
+    },
+    methods: {
+        onReset() {
+            this.$message.success("重置成功")
+        },
+        onSubmit({ validateResult, firstError }) {
+            const { state, mark } = this
+            if (validateResult === true) {
+                const result = state.filter((i) => i.id === mark)
+                if (result.length){
+                    console.log(result)
+                    const {name,type,government,id,address} = result[0]
+                    this.formData = {
+                        layout:"vertical",
+                        id,
+                        name,
+                        type,
+                        government,
+                        address
+                    }
+                    // console.log(result)
+                    this.$message.success("查询成功")
+                }
+                else  this.$message.warning("无结果")
+            } else {
+                console.log("Errors: ", validateResult)
+                this.$message.warning(firstError)
+            }
+        },
+        nameValidator(val) {
+            // eslint-disable-next-line no-restricted-globals
+            if (isNaN(val * 1)) return { result: false, message: "纳税人识别号应为数字组合！", type: "error" }
+            if (val.length < 5) return { result: false, message: "纳税人识别号应为大于4位数的组合", type: "error" }
+            this.mark = val
+            return { result: true, message: "c成功", type: "succeed" }
+            // if () return { result: false, message: "识别号长度错误！", type: "error" }
+        }
+    }
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ src/pages/test1/index.vue
@@ -0,0 +1,136 @@
+<template>
+    <div class="tdesign-demo-block-column tdesign-demo__table" style="width: 100%">
+        <!-- :expanded-row-keys="expandedRowKeys" 通过keys指定默认会展开的行-->
+        <t-table
+            :row-key="myProps.rowkey"
+            :columns="columns"
+            :data="data"
+            :expanded-row="expandedRow"
+            :expandIcon="expandIcon"
+            :expandOnRowClick="expandOnRowClick"
+            :horizontalScrollAffixedBottom="true"
+            table-layout="auto"
+            tableContentWidth="1200"
+            @expand-change="rehandleExpandChange"
+        >
+        </t-table>
+    </div>
+</template>
+
+<script lang="jsx">
+import Line1 from "./components/line1.vue"
+
+const getColumns = (isFixedColumn) => [{ colKey: "instance", title: " ", fixed: isFixedColumn ? "left" : "" }]
+
+const data = [
+    {
+        id: 100,
+        instance: "纳税人基本信息"
+    },
+    {
+        id: 101,
+        instance: "异常信息显示"
+    },
+    {
+        id: 102,
+        instance: "基本信息"
+    },
+    {
+        id: 103,
+        instance: "附送资料"
+    }
+]
+const rowkey = data.id
+
+export default {
+    data() {
+        return {
+            expandIcon: true,
+            expandOnRowClick: true,
+            fixedColumns: false,
+            emptyData: false,
+            data,
+            myProps: {
+                list: [],
+                rowkey
+            },
+            dataLoading: false,
+            expandedRow: (h, { row }) => <Line1 myProps={this.myProps} />
+        }
+    },
+    computed: {
+        columns() {
+            return getColumns(this.fixedColumns)
+        }
+    },
+    mounted() {
+        this.dataLoading = true
+        this.$request
+            .get("/api/get-info")
+            .then((res) => {
+                if (res.code === 0) {
+                    const { list = [] } = res.data
+                    this.myProps.list = list
+                    console.log("succeed", this.myProps)
+                }
+            })
+            .catch((e) => {
+                console.log(e)
+            })
+            .finally(() => {
+                this.dataLoading = false
+            })
+    },
+    methods: {
+        rehandleClickOp(data) {
+            console.log("data", data)
+        },
+        rehandleExpandChange(value, params) {
+            this.expandedRowKeys = value
+            console.log("rehandleExpandChange", params)
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+/deep/ [class*="t-table-expandable-icon-cell"] .t-icon {
+    background-color: transparent;
+}
+.link {
+    cursor: pointer;
+    margin-right: 15px;
+}
+.status {
+    position: relative;
+    color: #00a870;
+    margin-left: 10px;
+    &::before {
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translateY(-50%);
+        content: "";
+        background-color: #00a870;
+        width: 6px;
+        height: 6px;
+        margin-left: -10px;
+        border-radius: 50%;
+    }
+}
+.status.unhealth {
+    color: #e34d59;
+    &::before {
+        background-color: #e34d59;
+    }
+}
+.more-detail {
+    > p {
+        display: inline-block;
+        margin: 5px;
+    }
+    > p.title {
+        width: 100px;
+    }
+}
+</style>
